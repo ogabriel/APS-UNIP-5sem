40ff12f93a5ff0e4c3a64575c62e9880aff4685e
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-05-12 18:29:10 -0300

    adding the login form

---
+++ src/chatform/Login.java
@@ -0,0 +1,127 @@
+package chatform;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 948747724372712259L;
+	private JPanel contentPane;
+	private JTextField inputIP;
+	private JTextField inputPort;
+	private JTextField inputUser;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 374, 277);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Client client = new Client();
+				// TODO: send the data or the connection
+				// Connection con = connect();
+				// frame.connection(con)
+				client.setAlwaysOnTop (true);
+				setFocusableWindowState(false);
+				client.setVisible(true);
+			}
+		});
+		
+		btnLogin.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnLogin.setBounds(5, 198, 355, 39);
+		contentPane.add(btnLogin);
+		
+		JLabel lblServerIp = new JLabel("IP do servidor:");
+		lblServerIp.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblServerIp.setBounds(5, 11, 151, 39);
+		contentPane.add(lblServerIp);
+		
+		inputIP = new JTextField();
+		inputIP.setText("127.0.0.1");
+		inputIP.setFont(new Font("Arial", Font.PLAIN, 15));
+		inputIP.setBounds(138, 15, 222, 30);
+		contentPane.add(inputIP);
+		inputIP.setColumns(10);
+		
+		JLabel lblPortaDoServidor = new JLabel("Porta do servidor:");
+		lblPortaDoServidor.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblPortaDoServidor.setBounds(5, 72, 151, 39);
+		contentPane.add(lblPortaDoServidor);
+		
+		inputPort = new JTextField();
+		inputPort.setText("4444");
+		inputPort.setFont(new Font("Arial", Font.PLAIN, 15));
+		inputPort.setColumns(10);
+		inputPort.setBounds(138, 76, 222, 30);
+		contentPane.add(inputPort);
+		
+		JLabel lblUsurio = new JLabel("Nome de usu\u00E1rio:");
+		lblUsurio.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblUsurio.setBounds(5, 134, 151, 39);
+		contentPane.add(lblUsurio);
+		
+		inputUser = new JTextField();
+		inputUser.setText("unip1");
+		inputUser.setFont(new Font("Arial", Font.PLAIN, 15));
+		inputUser.setColumns(10);
+		inputUser.setBounds(138, 138, 222, 30);
+		contentPane.add(inputUser);
+	}
+	
+	private void connect() {
+		// TODO: create the connection with the data to send
+		// Create(getIP(), getPort(), getUser())
+	}
+	
+	private String getIP() {
+		return inputIP.getText();
+	}
+	
+	private String getPort() {
+		return inputPort.getText();
+	}
+	
+	private String getUser() {
+		return inputUser.getText();
+	}
+	
+	
+	
+	
+}
