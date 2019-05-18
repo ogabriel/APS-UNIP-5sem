903c9ec3441c400afde12cf0462147b238b7cd12
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-05-18 19:24:24 -0300

    formating chatform/client and fixing methods

--- src/chatform/Client.java
+++ src/chatform/Client.java
@@ -28,194 +28,209 @@ import java.net.Socket;
 
 public class Client extends JFrame {
 
-	/**
-	 * Socket variables
-	 */
-	private Socket socket;
-	private OutputStream ou ;
-	private Writer ouw;
-	private BufferedWriter bfw;	
-	
-	/**
-	 * Form variables
-	 */
-	private static final long serialVersionUID = 5391582161763137020L;
-	private JPanel contentPane;
-	private JTextField inputText;
-	private JScrollPane messages;
-	private JTextArea output;
+    /**
+     * Socket variables
+     */
+    private Socket socket;
+    private OutputStream outS;
+    private Writer outSWriter;
+    private BufferedWriter bufferWriter;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					// TODO: get the client username
-					Client frame = new Client();
-					frame.setVisible(true);
-					frame.establishConnection();
-					frame.listenConnection();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Client variables
+     */
+    private String user;
+    private String serverIP;
+    private int serverPort;
 
-	/**
-	 * Create the frame.
-	 */
-	public Client() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setLocationRelativeTo(null);
-		setTitle("Aplicação de Conversa (Cliente)");
-		
-		output = new JTextArea();
-		output.setEditable(false);
-		
-		messages = new JScrollPane(output);
-		messages.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-		contentPane.add(messages, BorderLayout.CENTER);
-		
-		JPanel interactive = new JPanel();
-		contentPane.add(interactive, BorderLayout.SOUTH);
-		interactive.setLayout(new BorderLayout(0, 0));
-		
-		inputText = new JTextField();
-		interactive.add(inputText, BorderLayout.CENTER);
-		inputText.setColumns(10);
-		
-		JPanel buttons = new JPanel();
-		interactive.add(buttons, BorderLayout.EAST);
-		buttons.setLayout(new BorderLayout(0, 0));
-		
-		JButton btnSend = new JButton("Enviar");
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					sendMessage(getText());
-				} catch (IOException e1) {
-					writeOutput("CAGOU");
-					e1.printStackTrace();
-				}
-			}
-		});
-		buttons.add(btnSend, BorderLayout.NORTH);
-		
-		JButton btnEnviarArquivo = new JButton("Enviar arquivo");
-		btnEnviarArquivo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				sendFile();
-			}
-		});
-		
-		inputText.addKeyListener(new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-					try {
-						sendMessage(getText());
-					} catch (IOException e1) {
-						writeOutput("CAGOU");
-						e1.printStackTrace();
-					}
-				}
-			}
-		});
-		
-		
-		buttons.add(btnEnviarArquivo, BorderLayout.CENTER);
-		
-		JButton btnExit = new JButton("Desconectar");
-		buttons.add(btnExit, BorderLayout.SOUTH);
-	}
-	
-	private void sendMessage(String msg) throws IOException {
-		// TODO: send the result to the method that sends the text
-		// LIKE: Send(getText());
-		if(msg.equals("Sair")) {
-			bfw.write("Desconectado \r\n");
-			writeOutput("Desconectado \r\n");
-		}else {
-			bfw.write(msg+"\r\n");
-			writeOutput( "meu pau" + " diz -> " + getText()+"\r\n");
-		}
-		bfw.flush();
-		
-		writeOutput(getText());
-		inputText.setText("");
-	}
-	
-	private void sendFile() {
-		// TODO: send the result to the method that sends the file
-		// LIKE: Send(getFile());
-		writeOutput("Você enviou o arquivo: " + getFile());
-	}
-	
-	private String getText() {
-		return inputText.getText();
-	}
-	
-	private void writeOutput(String phrase) {
-		output.append(phrase + "\r\n");
-	}
-	
-	private String getFile() {
-		JFileChooser chooseFile = new JFileChooser();
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("Apenas arquivos de texto e imagens", "jpg", "txt", "png", "xml");
-		chooseFile.setCurrentDirectory(new java.io.File("."));
-		chooseFile.setAcceptAllFileFilterUsed(false);
-		chooseFile.setFileFilter(filter);
-		String file = "";
-		
-		if(chooseFile.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-			file = chooseFile.getSelectedFile().getAbsolutePath(); 
-		}
-		return file;
-	}
-	
-	private void establishConnection() throws IOException {
-		socket = new Socket("127.0.0.1", 4444);
-	    ou = socket.getOutputStream();
-	    ouw = new OutputStreamWriter(ou);
-	    bfw = new BufferedWriter(ouw);
-	    bfw.write("minha pica" + "\r\n");
-	    bfw.flush();
-	}
-	
-	private void listenConnection() throws IOException {
-		InputStream in = socket.getInputStream();
-	    InputStreamReader inr = new InputStreamReader(in);
-	    BufferedReader bfr = new BufferedReader(inr);
-	    String msg = "";
-	    
-	    while(!"Sair".equalsIgnoreCase(msg)) {
-	    	if(bfr.ready()) {
-	    		msg = bfr.readLine();
-	    		if(msg.equals("Sair"))
-	    			output.append("Servidor caiu! \r\n");
-	    		else
-	    			output.append(msg+"\r\n");
-	    	}
-	    }
-	}
+    /**
+     * Form variables
+     */
+    private static final long serialVersionUID = 5391582161763137020L;
+    private JPanel contentPane;
+    private JTextField inputText;
+    private JScrollPane messages;
+    private JTextArea output;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) throws IOException {
+        Client frame = new Client();
+        frame.setClientInfo(args[0], args[1], args[2]);
+        frame.establishConnection();
+        frame.listenConnection();
+    }
+
+    /**
+     * Create the frame.
+     */
+    public Client() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+                                   @Override
+                                   public void windowClosing(WindowEvent e) {
+                                       Start start = new Start();
+                                       start.setVisible(true);
+                                       dispose();
+                                   }
+                               }
+        );
+        setBounds(100, 100, 600, 500);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setLocationRelativeTo(null);
+        setTitle("Aplicação de Conversa (Cliente)");
+
+        output = new JTextArea();
+        output.setEditable(false);
+
+        messages = new JScrollPane(output);
+        messages.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+        contentPane.add(messages, BorderLayout.CENTER);
+
+        JPanel interactive = new JPanel();
+        contentPane.add(interactive, BorderLayout.SOUTH);
+        interactive.setLayout(new BorderLayout(0, 0));
+
+        inputText = new JTextField();
+        interactive.add(inputText, BorderLayout.CENTER);
+        inputText.setColumns(10);
+
+        JPanel buttons = new JPanel();
+        interactive.add(buttons, BorderLayout.EAST);
+        buttons.setLayout(new BorderLayout(0, 0));
+
+        JButton btnSend = new JButton("Enviar");
+        btnSend.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    sendMessage(getText());
+                } catch (IOException e1) {
+                    writeOutput("Disconnected");
+                    e1.printStackTrace();
+                }
+            }
+        });
+        buttons.add(btnSend, BorderLayout.NORTH);
+
+        JButton btnEnviarArquivo = new JButton("Enviar arquivo");
+        btnEnviarArquivo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                sendFile();
+            }
+        });
+
+        inputText.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    try {
+                        sendMessage(getText());
+                    } catch (IOException e1) {
+                        writeOutput("CAGOU");
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+        buttons.add(btnEnviarArquivo, BorderLayout.CENTER);
+
+        JButton btnExit = new JButton("Desconectar");
+        buttons.add(btnExit, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+    public void setClientInfo(String user, String serverIP, String serverPort) {
+        this.user = user;
+        this.serverIP = serverIP;
+        this.serverPort = Integer.parseInt(serverPort);
+    }
+
+    private void sendMessage(String msg) throws IOException {
+        if (msg.equals("Disconnect")) {
+            bufferWriter.write(concatMsg("Desconectado"));
+        } else {
+            bufferWriter.write(concatMsg(msg));
+        }
+        bufferWriter.flush();
+        inputText.setText("");
+    }
+
+    private String concatMsg(String msg) {
+        return "[" + user + "]" + " -> " + msg + "\r\n";
+    }
+
+    private void sendFile() {
+        // TODO: send the result to the method that sends the file
+        // LIKE: Send(getFile());
+        writeOutput(concatMsg("Você enviou o arquivo: " + getFile()));
+    }
+
+    private String getText() {
+        return inputText.getText();
+    }
+
+    private void writeOutput(String phrase) {
+        output.append(phrase + "\r\n");
+    }
+
+    private String getFile() {
+        JFileChooser chooseFile = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Apenas arquivos de texto e imagens", "jpg", "txt", "png", "xml");
+        chooseFile.setCurrentDirectory(new java.io.File("."));
+        chooseFile.setAcceptAllFileFilterUsed(false);
+        chooseFile.setFileFilter(filter);
+        String file = "";
+
+        if (chooseFile.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            file = chooseFile.getSelectedFile().getAbsolutePath();
+        }
+        return file;
+    }
+
+    public void establishConnection() throws IOException {
+        socket = new Socket(this.serverIP, this.serverPort);
+//        socket = new Socket("127.0.0.1", 45454);
+        outS = socket.getOutputStream();
+        outSWriter = new OutputStreamWriter(outS);
+        bufferWriter = new BufferedWriter(outSWriter);
+        bufferWriter.write(user + "\r\n");
+        bufferWriter.flush();
+    }
+
+    public void listenConnection() throws IOException {
+        InputStream inS = socket.getInputStream();
+        InputStreamReader inSReader = new InputStreamReader(inS);
+        BufferedReader bufferedReader = new BufferedReader(inSReader);
+        String msg = "";
+
+        while (!"Disconnect".equalsIgnoreCase(msg)) {
+            if (bufferedReader.ready()) {
+                msg = bufferedReader.readLine();
+                output.append(msg + "\r\n");
+                if (msg.equals("Disconnect"))
+                    writeOutput("Desconectado do servidor...");
+                else
+                    writeOutput(msg);
+            }
+            break;
+        }
+    }
 }
