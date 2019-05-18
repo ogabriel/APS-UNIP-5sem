f4365e29f5506fd63011f0b7e817f1d3d2a507be
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-05-18 19:14:36 -0300

    fixing formatation connection/server

--- src/connection/Server.java
+++ src/connection/Server.java
@@ -1,59 +1,73 @@
 package connection;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.Buffer;
 import java.util.ArrayList;
 
-public class Server extends Thread{
-	private static ArrayList<BufferedWriter> clients;
-	// private static ArrayList<String> users;
-	private Socket con;
-	private InputStream in;
-	private InputStreamReader inr;
-	private BufferedReader bfr;
+public class Server extends Thread {
+    private static ArrayList<BufferedWriter> clients = new ArrayList<>();
+    private static ArrayList<String> users = new ArrayList<>();
+    private Socket connection;
+    private InputStream inS;
+    private InputStreamReader inSReader;
+    private BufferedReader bufferReader;
 
-	public Server (Socket con) throws IOException {
-		this.con = con;
-		this.clients = new ArrayList<>();
-		// this.users = new ArrayList<>();
+    public Server(Socket connection) throws IOException {
+        this.connection = connection;
 
-		// Writter
-		OutputStream ou = this.con.getOutputStream();
-		Writer ouw = new OutputStreamWriter(ou);
-		BufferedWriter bfw = new BufferedWriter(ouw);
-
-		clients.add(bfw);
-
-		try {
-			// Reader
-            this.in = con.getInputStream();
-            this.inr = new InputStreamReader(this.in);
-            this.bfr = new BufferedReader(this.inr);
+        try {
+            // Reader
+            inS = connection.getInputStream();
+            inSReader = new InputStreamReader(inS);
+            bufferReader = new BufferedReader(inSReader);
         } catch (IOException e) {
             e.printStackTrace();
         }
-	}
+    }
 
-	@Override
-	public void run() {
-		try {
-			String msg;
+    @Override
+    public void run() {
+        try {
 
-            while(true) {
-                msg = this.bfr.readLine();
-				System.out.println(msg);
-                this.broadCast(msg);
+            OutputStream ouS = this.connection.getOutputStream();
+            Writer osWriter = new OutputStreamWriter(ouS);
+            BufferedWriter bufferWriter = new BufferedWriter(osWriter);
+
+            clients.add(bufferWriter);
+            String current_user = this.bufferReader.readLine();
+            users.add(current_user);
+            String msg = "";
+
+            while (!"Disconnect".equalsIgnoreCase(msg) && msg != null) {
+                msg = this.bufferReader.readLine();
                 System.out.println(msg);
+                // close the thread
+                try {
+                    bufferWriter.flush();
+                } catch (IOException e) {
+                    int index = users.indexOf(current_user);
+                    clients.remove(index);
+                    users.remove(index);
+                    break;
+                }
+                this.broadCast(bufferWriter, msg);
             }
         } catch (Exception e) {
-			e.printStackTrace();
+            e.printStackTrace();
         }
-	}
+    }
 
-	public void broadCast(String msg) throws IOException {
-		for(BufferedWriter bw : clients) {
-			bw.write(msg + "\r\n");
-			bw.flush();
-		}
-	}
+    public void broadCast(BufferedWriter bInput, String msg) throws IOException {
+        BufferedWriter bufferWriterOut;
+
+        for (BufferedWriter bufferClient : clients) {
+            bufferWriterOut = bufferClient;
+            if (!(bufferWriterOut == bInput)) {
+                bufferClient.write(msg + "\r\n");
+                bufferClient.flush();
+            }
+        }
+    }
 }
