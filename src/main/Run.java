d92b539dea69783ac1a1b83a642362b2ccd21f09
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-05-18 00:09:26 -0300

    renaming in the starting class

--- src/main/Run.java
+++ src/main/Run.java
@@ -1,11 +1,11 @@
 package main;
 
-import chatform.Inicio;
+import chatform.Start;
 
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Inicio.main(null);
+		Start.main(null);
 	}
 }
